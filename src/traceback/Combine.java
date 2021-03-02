63ca1f1bc92b6bafbbf289f3f5cff34275ff91d9
wangyouwei <2368527390@qq.com>
2021-03-02 16:43:40 +0800

    使用动态规划去解决机器人的障碍路径问题

--- src/traceback/Combine.java
+++ src/traceback/Combine.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Combine {
-    public List<List<Integer>> combine(int n, int k) {
+    public static List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new LinkedList<>();
         if ( k == 0 ) {
             return res;
@@ -14,15 +14,20 @@ public class Combine {
         helper( n, 1, k, path, res );
         return res;
     }
-    private void helper( int n, int index, int depth, List<Integer> path, List<List<Integer>> res ) {
+    private static void helper( int n, int index, int depth, List<Integer> path, List<List<Integer>> res ) {
         if ( depth == 0 ) {
             res.add( new ArrayList<>(path));
             return ;
         }
         for ( int i = index; i <= n; i++ ) {
-            path.add( index );
+            path.add( i );
             helper( n, index+1, depth-1, path, res );
             path.remove( path.size() - 1 );
         }
+    }
+
+    public static void main(String[] args) {
+        int k = 2, n = 4;
+        combine( n, k );
     }
 }
